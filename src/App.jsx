0ee3f8d2a1aadc4b287a6fd7c4146b5d716d90e5
Thyyd <roastingblade@gmail.com>
2026-07-23 00:30:23 +0200

    Initialisation du composant App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Authentication from './routes/auth/Authentication';
+import Dashboard from './routes/dashboard/Dashboard';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    axios.post('/api/auth/', {}, {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+      .then(response => {
+        setIsLoggedIn(true);
+        setUserUsername(response.data.username);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-
+      {isLoggedIn ?
+        <Dashboard />:
+        <Authentication />
+      }
     </div>
   )
 }
